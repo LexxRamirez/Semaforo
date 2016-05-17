7f15706ea198871ad86b55499e24102045da31f5
LexxRamirez <antoni.the21@gmail.com>
2016-05-16 20:15:32 -0600

    new file:   semaforo.ino

---
+++ Semaforo/semaforo.ino
@@ -0,0 +1,28 @@
+int red = 4;
+int yellow = 6;
+int green = 8;
+
+void setup(){
+  pinMode(red,OUTPUT);
+  pinMode(yellow,OUTPUT);
+  pinMode(green,OUTPUT);
+}
+
+void loop(){
+  digitalWrite(red, HIGH);
+  display(5000);
+  digitalWrite(red, LOW);
+  display(500);
+  
+  for(int i=0; i){
+  digitalWrite(yellow, HIGH);
+  display(1000);
+  digitalWrite(yellow, LOW);
+  display(500);
+  }
+  digitalWrite(green, HIGH);
+  display(5000);
+  digitalWrite(green, LOW);
+  display(500);
+  
+}
